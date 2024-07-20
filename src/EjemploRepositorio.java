7f78cecae9cecd68b0efc6b5d5e906cf98b23d00
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-20 16:26:39 -0500

    class EjemploRepositorio: probando el metodo ordenable

--- src/EjemploRepositorio.java
+++ src/EjemploRepositorio.java
@@ -3,6 +3,8 @@ import java.util.List;
 import org.jhonatan.pooInterface.Modelo.Cliente;
 import org.jhonatan.pooInterface.Repositori.ClienteListRepositorio;
 import org.jhonatan.pooInterface.Repositori.CrudRepositorio;
+import org.jhonatan.pooInterface.Repositori.Direccion;
+import org.jhonatan.pooInterface.Repositori.OrdenableRepositorio;
 import org.jhonatan.pooInterface.Repositori.PaginableRepositorio;
 
 public class EjemploRepositorio {
@@ -14,15 +16,22 @@ public class EjemploRepositorio {
     public static void EjemploRepositorio() {
         CrudRepositorio repo = new ClienteListRepositorio();
         repo.crear(new Cliente("Jano", "Perez"));
-        repo.crear(new Cliente("Juana", "Gonzalez"));
+        repo.crear(new Cliente("Andrez", "Gonzalez"));
         repo.crear(new Cliente("luci", "Martinez"));
         repo.crear(new Cliente("Jhonatan", "Isai"));
 
         // listamo
         List<Cliente> clientes = repo.listar();
         clientes.forEach(System.out::println);
+
         System.out.println("=====PAGINABLE====");
         List<Cliente> paginable = ((PaginableRepositorio) repo).listar(0, 3);
         paginable.forEach(System.out::println);
+
+        System.out.println("=====ORDENAR=====");
+        List<Cliente> clientesOrdenable = ((OrdenableRepositorio) repo).listar("nombre", Direccion.ASC);
+        for (Cliente cliente : clientesOrdenable) {
+            System.out.println(cliente);
+        }
     }
 }
