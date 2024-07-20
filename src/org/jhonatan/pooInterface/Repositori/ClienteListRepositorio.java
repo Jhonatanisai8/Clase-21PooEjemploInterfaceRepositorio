5ec695b0ec845885ac2b00ac298026f447e57824
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-20 15:34:35 -0500

    class ClienteListRepositorio: clase que les va a dar funcionalidad a los metodos de la interface que se le asigna

---
+++ src/org/jhonatan/pooInterface/Repositori/ClienteListRepositorio.java
@@ -0,0 +1,39 @@
+package org.jhonatan.pooInterface.Repositori;
+
+import java.util.List;
+
+import org.jhonatan.pooInterface.Modelo.Cliente;
+
+public class ClienteListRepositorio implements CrudRepositorio {
+
+    @Override
+    public List<Cliente> listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+
+    @Override
+    public Cliente porId(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'porId'");
+    }
+
+    @Override
+    public void crear(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+    }
+
+    @Override
+    public void editar(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'editar'");
+    }
+
+    @Override
+    public void eliminar(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+}
