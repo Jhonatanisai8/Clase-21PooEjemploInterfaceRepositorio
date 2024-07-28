97d55ad79a89124d37ee8ece0dcc5043eac18983
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 15:04:56 -0500

    class ClientePremiun: agregando la implementacion de Comparable

--- src/org/jhonatan/pooInterface/Modelo/ClientePremiun.java
+++ src/org/jhonatan/pooInterface/Modelo/ClientePremiun.java
@@ -1,7 +1,13 @@
 package org.jhonatan.pooInterface.Modelo;
 
-public class ClientePremiun extends Cliente {
+public class ClientePremiun extends Cliente implements Comparable<ClientePremiun>{
     public ClientePremiun(String nombre, String apellido) {
         super(nombre, apellido);
     }
+
+    @Override
+    public int compareTo(ClientePremiun o) {
+        return 0;
+    }
+    
 }
