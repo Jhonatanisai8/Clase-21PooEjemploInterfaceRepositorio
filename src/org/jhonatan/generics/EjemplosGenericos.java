82aa1e01070a9b773704e1492dd12f21553e9924
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 14:39:42 -0500

    class EjemplosGenericos: otro metodo de tipo generico pero con dos parametros

--- src/org/jhonatan/generics/EjemplosGenericos.java
+++ src/org/jhonatan/generics/EjemplosGenericos.java
@@ -19,6 +19,7 @@ public class EjemplosGenericos {
 
         Integer[] enterosArreglo = { 1, 2, 3 };
 
+        System.out.println("===EJEMPLO USANDO EL PRIMER METODO===");
         List<Cliente> clientesLista = fromArrayList(clientesArreglo);
 
         List<Integer> enterosLista = fromArrayList(enterosArreglo);
@@ -26,10 +27,26 @@ public class EjemplosGenericos {
         clientesLista.forEach(System.out::println);
         enterosLista.forEach(System.out::println);
 
+        /* Ejemplos */
+        System.out.println("===EJEMPLOS USANDO EL SEGUNDO METODO ===");
+        List<String> nombres = fromArrayList(new String[] { "Andres",
+                "Juan", "Pedro", "Alberto" }, enterosArreglo);
+        nombres.forEach(System.out::println);
+
     }
 
     /* metodo de tipo generico */
     public static <T> List<T> fromArrayList(T[] c) {
+        return Arrays.asList(c);
+    }
+
+    /* metodo generico con dos parametros */
+    public static <T, G> List<T> fromArrayList(T[] c, G[] g) {
+        /* c convierte */
+        /* g itera e imprime */
+        for (G elemento : g) {
+            System.out.println(elemento);
+        }
         return Arrays.asList(c);
     }
 
