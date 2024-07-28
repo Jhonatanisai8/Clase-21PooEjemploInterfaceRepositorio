ff4145e02979fc630b1f3cf2c8ce53b96dc903b5
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 14:47:40 -0500

    class EjemplosGenericosConLimites: metodos genericos con limites

---
+++ src/org/jhonatan/generics/EjemplosGenericosConLimites.java
@@ -0,0 +1,57 @@
+package org.jhonatan.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jhonatan.pooInterface.Modelo.Cliente;
+
+public class EjemplosGenericosConLimites {
+    public static void main(String[] args) {
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Andrez", "Guzman"));
+
+        // optenemos el primer elemento
+        Cliente andres = clientes.iterator().next();
+
+        Cliente[] clientesArreglo = { new Cliente("Luci", "Martinez"),
+                new Cliente("Andrez", "Guzman") };
+
+        Integer[] enterosArreglo = { 1, 2, 3 };
+
+        System.out.println("===EJEMPLO USANDO EL PRIMER METODO===");
+        List<Cliente> clientesLista = fromArrayList(clientesArreglo);
+
+        List<Integer> enterosLista = fromArrayList(enterosArreglo);
+
+        clientesLista.forEach(System.out::println);
+        enterosLista.forEach(System.out::println);
+
+        /* Ejemplos */
+        System.out.println("===EJEMPLOS USANDO EL SEGUNDO METODO ===");
+        List<String> nombres = fromArrayList(new String[] { "Andres",
+                "Juan", "Pedro", "Alberto" }, enterosArreglo);
+        nombres.forEach(System.out::println);
+
+    }
+
+    public static <T> List<T> fromArrayList(T[] c) {
+        return Arrays.asList(c);
+    }
+
+    /* metodo de tipo generico en donde pasamos tipos de Number */
+    public static <T extends Number> List<T> fromArrayList(T[] c) {
+        return Arrays.asList(c);
+    }
+
+    /* metodo generico con dos parametros */
+    public static <T, G> List<T> fromArrayList(T[] c, G[] g) {
+        /* c convierte */
+        /* g itera e imprime */
+        for (G elemento : g) {
+            System.out.println(elemento);
+        }
+        return Arrays.asList(c);
+    }
+
+}
